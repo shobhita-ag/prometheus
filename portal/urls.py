d2214f18039c6351a3ec2e74b79f15751be7d446
Shobhita Agarwal <shobhita.agarwal@gmail.com>
2019-03-13 00:01:22 +0530

    Get/Update/Create order API and frontend implementation

--- portal/urls.py
+++ portal/urls.py
@@ -2,11 +2,17 @@ from django.conf.urls import url
 from portal import views
 
 urlpatterns = [
+	# views for page loading
     url(r'^$', views.HomePage.as_view(), name='home'),
-    url(r'^about$', views.AboutPage.as_view(), name='about'),
-    url(r'^fetchUserData$', views.fetchUserData.as_view(), name='fetchUserData'),
+    url(r'^login$', views.Login.as_view(), name='login'),
+    url(r'^logout$', views.Logout.as_view(), name='logout'),
+    url(r'^dashboard$', views.Dashboard.as_view(), name='dashboard'),
+    url(r'^create_edit_order$', views.CreateEditOrder.as_view(), name='create_edit_order'),
 
     #api calls for crud operations
-    url(r'^create_order$', views.create_order.as_view(), name='create_order'),
+    url(r'^dashboard/order_data$', views.DashboardOrders.as_view(), name='dashboard_orders'),
+    url(r'^create_edit_order/get_order$', views.GetOrder.as_view(), name='get_order'),
+    url(r'^create_edit_order/get_drop_down_data$', views.GetDropDownData.as_view(), name='get_drop_down_data'),
+    
+    #url(r'^create_order$', views.create_order.as_view(), name='create_order'),
 ]
-
